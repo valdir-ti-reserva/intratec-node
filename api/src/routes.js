42339ff0a68311c90916d7dafe5aee6a627f63db
Valdir Silva <valdir.ti@gmail.com>
2021-01-06 16:43:13 -0300

    feat: create API from bolierplate with Sequelize(mysql2) and MVC

---
+++ api/src/routes.js
@@ -0,0 +1,21 @@
+const { Router } = require('express');
+const routes = Router();
+
+// const AuthController = require('./controllers/AuthController');
+// const ProductController = require('./controllers/ProductController');
+// const UserController = require('./controllers/UserController');
+
+// const authMiddleware = require('./middlewares/Auth');
+routes.get('/', (req, res)=>{
+  res.send('API Running!')
+});
+
+// routes.post('/login', AuthController.index);
+// routes.post('/register', AuthController.store);
+
+// routes.get('/products', authMiddleware, ProductController.index);
+
+// routes.get('/users', UserController.index);
+// routes.delete('/users/:id', UserController.delete);
+
+module.exports = routes;
